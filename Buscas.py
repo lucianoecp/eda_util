ed78f3e8dde90db92ec3078bcef6d89de0820d45
Luciano S. de Arruda <71555478+lucianoecp@users.noreply.github.com>
2021-10-03 23:17:31 -0300

    update Buscas.py

--- Buscas.py
+++ Buscas.py
@@ -2,8 +2,21 @@ def busca_sequencial(lista, chave):
     for i in range(len(lista)):
         if lista[i] == chave:
             return i
+
     return -1	
 
-lista = [20, 5, 15, 24, 67, 45, 1, 76]
-print(busca_sequencial(lista, 24))
-print(busca_sequencial(lista, 100))
+
+def busca_binaria(lista, chave):
+    inicio = 0
+    fim = len(lista)-1
+    
+    while inicio <= fim:
+        meio = (inicio + fim)//2
+        if lista[meio] == chave:
+            return meio
+        elif lista[meio] > chave:
+            fim = meio - 1
+        else:
+            inicio = meio + 1
+
+    return -1
